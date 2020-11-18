d04c3b20068efb05cba82024639df221891622f3
jbeedle19 <josh.beedle@gmail.com>
2020-11-18 12:18:19 -0500

    handled repos with 30+ issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,6 @@
 // Variables:
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 // Functions:
 var getRepoIssues = function(repo) {
@@ -11,6 +12,11 @@ var getRepoIssues = function(repo) {
             response.json().then(function(data) {
                 // Pass response data to DOM function
                 displayIssues(data);
+
+                // Check if API has paginated issues (more than 30 issues)
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         } else {
             alert("There was a problem with your request!");
@@ -53,7 +59,19 @@ var displayIssues = function(issues) {
 
         issueContainerEl.appendChild(issueEl);
     }
-
 };
 
-getRepoIssues("jbeedle19/git-it-done");
+var displayWarning = function(repo) {
+    // Add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // Append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+getRepoIssues("angular/angular");
